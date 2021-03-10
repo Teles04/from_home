e64c37037400c5c6eb7625c029af9e8651e4b731
teles <teles.raa@gmail.com>
2021-03-10 07:34:28 +0300

    1003

--- drf-intro/simple_crud/measurements/models.py
+++ drf-intro/simple_crud/measurements/models.py
@@ -11,6 +11,6 @@ class Project(models.Model):
 
 class Measurement(models.Model):
     value = models.FloatField()
-    project = models.ForeignKey(Project, related_name='measurements', on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, related_name='measurements', on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- drf-intro/simple_crud/measurements/serializers.py
+++ drf-intro/simple_crud/measurements/serializers.py
@@ -3,13 +3,24 @@ from rest_framework import serializers
 from measurements.models import Project, Measurement
 
 
-class ProjectSerializer(serializers.ModelSerializer):
-    measurements = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-    class Meta:
-        model = Project
-        fields = 'id', 'name', 'latitude', 'longitude', 'created_at', 'updated_at', 'measurements',
-
 class MeasurementSerializer(serializers.ModelSerializer):
     class Meta:
         model = Measurement
         fields = 'id', 'value', 'created_at', 'updated_at',
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    measurements = MeasurementSerializer(many=True)
+
+    class Meta:
+        model = Project
+        fields = 'id', 'name', 'latitude', 'longitude', 'created_at', 'updated_at', 'measurements',
+
+    def create(self, validated_data):
+        measurements_data = validated_data.pop('measurements')
+        project = Project.objects.create(**validated_data)
+        for measurement_data in measurements_data:
+            Measurement.objects.create(project=project, **measurement_data)
+        return project
+
+
