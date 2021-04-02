3bfd23872b50c8977402be06be8f61dff8c9f451
teles <teles.raa@gmail.com>
2021-04-02 23:13:27 +0300

    seven_done

--- drf-auth-and-validation/api_with_restrictions/advertisements/views.py
+++ drf-auth-and-validation/api_with_restrictions/advertisements/views.py
@@ -1,5 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import status
+from rest_framework import status, permissions
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
@@ -9,26 +9,14 @@ from advertisements.serializers import AdvertisementSerializer
 
 from advertisements.filters import AdvertisementFilterSet
 
+from advertisements.permissions import IsOwnerOrReadOnly
+
 
 class AdvertisementViewSet(ModelViewSet):
     """ViewSet для объявлений."""
-
-    # TODO: настройте ViewSet, укажите атрибуты для кверисета,
-    #   сериализаторов и фильтров
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
     filter_backends = [DjangoFilterBackend]
     filter_class = AdvertisementFilterSet
-
-    def get_permissions(self):
-        """Получение прав для действий."""
-        if self.action in ["create", "update", "partial_update"]:
-            return [IsAuthenticated()]
-        return []
-
-    def destroy(self, request, *args, **kwargs):
-        if self.action in ["delete"]:
-            return [IsAuthenticated()]
-        instance = self.get_object()
-        self.perform_destroy(instance)
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                      IsOwnerOrReadOnly]
